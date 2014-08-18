44847801e67a3d475a0a42175b57b4829f66fe24
nelo360 <nelo360@gmail.com>
2014-08-18 10:53:17 +0200

    device specific locales
    Change-Id: Id4b003f59ef0b7efe68d42ed71e3c0e2aac189b8

--- slim.mk
+++ slim.mk
@@ -22,5 +22,7 @@ PRODUCT_MODEL := LG-D802
 PRODUCT_MANUFACTURER := lge
 PRODUCT_RESTRICT_VENDOR_FILES := false
 
+PRODUCT_LOCALES := es_ES ca_ES en_GB en_US en_IN fr_FR it_IT et_EE de_DE nl_NL cs_CZ pl_PL ja_JP zh_TW zh_CN zh_HK ru_RU ko_KR nb_NO es_US da_DK el_GR tr_TR pt_PT pt_BR rm_CH sv_SE bg_BG fi_FI hi_IN hr_HR hu_HU in_ID iw_IL lt_LT lv_LV ro_RO sk_SK sl_SI sr_RS uk_UA vi_VN tl_PH ar_EG fa_IR th_TH sw_TZ ms_MY af_ZA zu_ZA am_ET hi_IN en_XA ar_XB fr_CA km_KH lo_LA ne_NP si_LK mn_MN hy_AM az_AZ ka_GE
+
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=lge/g2_open_com/g2:4.2.2/JDQ39B/D80210a.1378316352:user/release-keys PRIVATE_BUILD_DESC="g2_open_com-user 4.2.2 JDQ39B D80210a.1378316352 release-keys"
 
